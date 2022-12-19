44ec5cb6fecbfbe692bd9e15d03852a871e5a135
aman <sethaman2021@gmail.com>
2022-12-19 20:24:58 +0530

    commit

--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,27 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import Todo from './todo';
+import App from './app';
 import './main.css';
+import Root from "./routes/root";
+
+
+const router = createBrowserRRouter([
+    {
+        path: '/',
+        element: <h1>Home Page</h1>,
+    },
+    {
+        path: '/about',
+        element: <h1>About Page</h1>,
+    },
+    {
+        path: '/contact',
+        element: <h1>Contact Page</h1>,
+    },
+]);
+
 
 const container = document.getElementById('root');
 const root = createRoot(container);
 
-root.render(<Todo />);
+root.render(<RouterProvider router= {router} />);
